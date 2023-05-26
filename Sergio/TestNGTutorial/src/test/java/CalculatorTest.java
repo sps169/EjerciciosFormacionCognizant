0b8f3e28096034bb8ae505a20f645208054dbc31
Perez Sanz <2283666@cognizant.com>
2023-05-26 10:18:13 +0200

    first test class

--- Sergio/TestNGTutorial/src/test/java/CalculatorTest.java
+++ Sergio/TestNGTutorial/src/test/java/CalculatorTest.java
@@ -1,17 +1,34 @@
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.asserts.Assertion;
 
 public class CalculatorTest {
+
+    private Calculator calc;
+
+    @BeforeTest
+    public void setUpCalculator() {
+        calc = new Calculator();
+    }
     @Test
     public void testSumValid(){
-        Calculator calc = new Calculator();
         Assert.assertEquals(calc.sum(1, 2), 1 + 2);
     }
 
     @Test
-    public void testSum() {
-        Calculator calc = new Calculator();
-        Assert.assertEquals(calc.sum(Integer.MAX_VALUE, 1), Integer.MAX_VALUE + 1);
+    public void testSubstractValid() {
+        Assert.assertEquals(calc.substract(1, -1), 2);
     }
+
+    @Test
+    public void testMultiplyValid() {
+        Assert.assertEquals(calc.multiply(2, 5), 10);
+    }
+
+    @Test
+    public void testDivideValid() {
+        Assert.assertEquals(calc.divide(1, 2), 1/2);
+    }
+
 }
